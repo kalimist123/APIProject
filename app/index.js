8f9450c51b03cfaca91e2516a1780365dc38eeb7
kalimist123 <kali.mist@gmail.com>
2018-10-26 23:40:47 +0100

    initial

---
+++ app/index.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var url = require('url')
+
+var server = http.createServer(function(req,res){
+    
+
+
+    var parsedUrl = url.parse(req.url, true);
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+    res.end('hello world\n');
+
+    console.log('Request received on path:' +trimmedPath);
+
+
+});
+
+server.listen(3000, function(){
+    console.log("The server is listening on port 3000 now");
+});
+
